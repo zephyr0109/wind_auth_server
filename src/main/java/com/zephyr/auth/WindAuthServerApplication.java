1b0bd0f736589f8dd0150e2ea0de6b17923096fa
junyul <star8076@hanmail.net>
2020-12-19 11:31:08 +0900

    init commit

---
+++ src/main/java/com/zephyr/auth/WindAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.zephyr.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WindAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WindAuthServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zephyr/auth/WindAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zephyr.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WindAuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
