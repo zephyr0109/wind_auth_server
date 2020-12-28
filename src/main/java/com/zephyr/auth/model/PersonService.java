08d0f67ea4f291633af23e122e9b1a6297d2c65e
jun <star8076@gmail.com>
2020-12-28 23:10:04 +0900

    Person 에 로그인 날짜, 수정 날짜, 생성 날짜 추가 PersonService에 수정, 로그인 관련 db 조회 기능 추가 관련 test 소스 추가, person 수정 기능은 진행 중

--- src/main/java/com/zephyr/auth/model/PersonService.java
+++ src/main/java/com/zephyr/auth/model/PersonService.java
@@ -2,13 +2,12 @@ package com.zephyr.auth.model;
 
 import static org.springframework.data.mongodb.core.query.Criteria.where;
 
-import java.util.Date;
+import java.time.LocalTime;
 
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
-import org.springframework.data.mongodb.core.query.UpdateDefinition;
 import org.springframework.stereotype.Service;
 
 import lombok.extern.slf4j.Slf4j;
@@ -24,10 +23,9 @@ public class PersonService {
 	}
 
 	public Person insertPerson(Person p) {
-
-		Person result = mongoOps.insert(p);
+		p.setRegisterDate(LocalTime.now());
+		Person result = mongoOps.insert(p);		
 		log.debug("get person info, id : " + result.getId());
-
 		return result;
 	}
 
@@ -59,10 +57,10 @@ public class PersonService {
 	public Person changePassword(String id, String oldPwd, String newPwd) {
 		final String key = "password";
 		final String key2 = "id";
-		Query criteria = new Query(Criteria.where(key2).is(id).and(key).is(oldPwd));		
-		Update update=  new Update().set(key, newPwd);
-		
-		Person p = mongoOps.update(Person.class).matching(criteria ).apply(update).findAndModifyValue();
+		Query criteria = new Query(Criteria.where(key2).is(id).and(key).is(oldPwd));
+		Update update = new Update().set(key, newPwd);
+
+		Person p = mongoOps.update(Person.class).matching(criteria).apply(update).findAndModifyValue();
 
 		return p;
 	}
@@ -70,6 +68,35 @@ public class PersonService {
 	public Person findById(String id) {
 		// TODO Auto-generated method stub
 		return mongoOps.findById(id, Person.class);
+	}
+
+	public Person findByIdAndPassword(Person p) {
+
+		Query query = new Query();
+		Criteria c = Criteria.where("id").is(p.getId()).and("password").is(p.getPassword());
+		query.addCriteria(c);
+		
+		return mongoOps.findOne(query, Person.class);
+	}
+
+	/**
+	 * 로그아웃 함수
+	 * @param string 로그아웃 id
+	 * @return 성공적으로 로그아웃 했을 경우 true
+	 */
+	public boolean signOut(String string) {
+		// TODO: 1. 로그인 히스토리 db 생성 시 내용 기록
+		
+		// TODO: 2. spring security 적용 후 인증토큰 생성 시 해당 토큰 파기
+		return true;
+	}
+
+	public Person modifyPerson(String id, Person changePerson) {
+		// TODO: 입력한 값이 null이 아닌 값만 변경해야 함.
+		
+		
+		
+		return null;
 	}
 
 }
--- src/test/java/com/zephyr/auth/model/PersonServiceTest.java
+++ src/test/java/com/zephyr/auth/model/PersonServiceTest.java
@@ -3,6 +3,7 @@ package com.zephyr.auth.model;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertNotNull;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,15 +18,15 @@ import lombok.extern.slf4j.Slf4j;
 @ContextConfiguration(classes = MongoConfigForTest.class)
 class PersonServiceTest {
 
-	
 	PersonService personService;
-	
+
 	@Autowired
 	MongoTemplate mongoOps;
-	
+
 	@BeforeEach
-	void init() {	
-		personService = new PersonService(mongoOps);		
+	void before() {
+		personService = new PersonService(mongoOps);
+		System.out.println(mongoOps);
 	}
 
 	@Test
@@ -37,30 +38,56 @@ class PersonServiceTest {
 
 	@Test
 	void insertPerseon() {
+		mongoOps.dropCollection(Person.class);
+
 		Person p = Person.builder().id("tuser").name("zep").email("star8076@hanmail.net").password("test1").build();
 		Group group = new Group();
 		group.setGroupId("tid1");
 		group.setName("test group");
-		p.setGroup(group );
+		p.setGroup(group);
 		Person result = personService.insertPerson(p);
 		assertThat(p.getName()).isEqualTo(result.getName());
 		log.info(result.toString());
 
 	}
-	
+
 	@Test
 	void changePassword() {
 		String newPwd = "test2";
 		final String oldPwd = "test1";
-		Person oldP =personService.changePassword("tuser", oldPwd, newPwd );
+		Person oldP = personService.changePassword("tuser", oldPwd, newPwd);
 		log.info(oldP.toString());
 		assertThat(oldP.getPassword()).isEqualTo(oldPwd);
 		Person newP = personService.findById("tuser");
 		assertThat(newP.getPassword()).isEqualTo(newPwd);
-		
-		
-		
-		
+	}
+
+	@Test
+	void signIn() {
+		Person p = Person.builder().id("tuser").password("test1").build();
+		Person result = personService.findByIdAndPassword(p);
+		assertThat(result).isNotNull();
+		assertThat(result.getId()).isEqualTo(p.getId());
+		p.setPassword("wrongPwd");
+		result = personService.findByIdAndPassword(p);
+		assertThat(result).isNull();
+	}
+
+	@Test
+	void signOut() {
+
+		boolean flag = personService.signOut("test1");
+		assertThat(flag).isTrue();
+	}
+
+	@Test
+	void modifyPerson() {
+//		실패남
+		Person changePerson = personService.findById("tuser");
+		changePerson.setEmail("change@hanmail.net");
+		Person newPerson = personService.modifyPerson("tuser", changePerson);
+		assertThat(changePerson.getEmail()).isEqualTo(newPerson.getEmail());
+		assertThat(changePerson.getId()).isEqualTo(newPerson.getId());
 	}
 
 }
